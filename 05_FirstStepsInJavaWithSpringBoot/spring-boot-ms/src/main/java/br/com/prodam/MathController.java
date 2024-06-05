10281d5225173b96d797e303d2d530d975009ca5
Anderson Abreu do Nascimento <anderson.a.nascimento@gmail.com>
2024-06-05 08:36:14 -0400

    Iniciando versionamento

---
+++ 05_FirstStepsInJavaWithSpringBoot/spring-boot-ms/src/main/java/br/com/prodam/MathController.java
@@ -0,0 +1,39 @@
+package br.com.prodam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+	private static final String template = "Hello %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping(value = "/sum/{num1}/{num2}", method = RequestMethod.GET)
+	public Double sum(@PathVariable(value = "num1") String num1, @PathVariable(value = "num2") String num2)
+			throws Exception {
+		if (!isNumeric(num1) || !isNumeric(num2)) {
+			throw new Exception();
+		}
+		return convertToDouble(num1) + convertToDouble(num2);
+	}
+
+	private Double convertToDouble(String strNumber) {
+		if (strNumber == null)
+			return 0D;
+		String number = strNumber.replaceAll(",", ".");
+		if (isNumeric(number))
+			return Double.parseDouble(number);
+		return 0D;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		if (strNumber == null)
+			return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+	}
+}
