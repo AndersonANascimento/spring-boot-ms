f7c87df33775c02f275129c4f1e0ff58adc7e78c
Anderson Abreu do Nascimento <anderson.a.nascimento@gmail.com>
2024-06-07 20:06:13 -0400

    Adiciona acesso ao repositório e remove Mocks

--- 05_FirstStepsInJavaWithSpringBoot/spring-boot-ms/src/main/java/br/com/prodam/exceptions/handler/CustomizeResponseEntityExceptionHandler.java
+++ 05_FirstStepsInJavaWithSpringBoot/spring-boot-ms/src/main/java/br/com/prodam/exceptions/handler/CustomizeResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.prodam.exceptions.ExceptionResponse;
+import br.com.prodam.exceptions.ResourceNotFoundException;
 import br.com.prodam.exceptions.UnsupportedMathOperationException;
 
 @ControllerAdvice
@@ -27,5 +28,11 @@ public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExcep
     public final ResponseEntity<ExceptionResponse> handleBadRequestException(Exception e, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public final ResponseEntity<ExceptionResponse> handleNotFoundException(Exception e, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), e.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 }
--- 05_FirstStepsInJavaWithSpringBoot/spring-boot-ms/src/main/java/br/com/prodam/services/PersonServices.java
+++ 05_FirstStepsInJavaWithSpringBoot/spring-boot-ms/src/main/java/br/com/prodam/services/PersonServices.java
@@ -1,62 +1,55 @@
 package br.com.prodam.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.prodam.exceptions.ResourceNotFoundException;
 import br.com.prodam.model.Person;
+import br.com.prodam.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
-    private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    @Autowired
+    PersonRepository repository;
 
     public List<Person> findAll() {
         logger.info("Buscando todas as pessoas...");
-        List<Person> persons = new ArrayList<>();
-        for (int i = 0; i < 8; i++) {
-            Person person = mockPerson(i);
-            persons.add(person);
-        }
-        return persons;
+        return repository.findAll();
     }
 
-    public Person findById(String id) {
+    public Person findById(Long id) {
         logger.info("Buscando uma pessoa por id...");
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Anderson");
-        person.setLastName("Nascimento");
-        person.setAddress("Manaus-AM, Brasil");
-        person.setGender("Masculino");
-        return person;
+        return repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Registro não encontrado para este ID!"));
     }
-    
+
     public Person create(Person person) {
         logger.info("Criando uma pessoa...");
-        return person;
+        return repository.save(person);
     }
 
     public Person update(Person person) {
         logger.info("Atualizando uma pessoa...");
-        return person;
+        Person entity = repository.findById(person.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Registro não encontrado para este ID!"));
+
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+
+        return repository.save(person);
     }
 
-    public void delete(String id) {
+    public void delete(Long id) {
         logger.info("Deletando uma pessoa...");
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Registro não encontrado para este ID!"));
+        repository.delete(entity);
     }
-
-    private Person mockPerson(int i) {
-        Person person = new Person();
-        person.setId(counter.incrementAndGet());
-        person.setFirstName("Name " + i);
-        person.setLastName("LastName " + i);
-        person.setAddress("Address " + i);
-        person.setGender("Masculino");
-        return person;
-    }
-
 }
